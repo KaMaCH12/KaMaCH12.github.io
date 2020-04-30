883aaabc548b988ffc1374879c80f44517eba4f9
KaMaCH12 <kamachos12@gmail.com>
2020-05-01 01:43:08 +0200

    Quick fix

--- scripts/emotes_on_click.js
+++ scripts/emotes_on_click.js
@@ -3,13 +3,12 @@ function emote()
 {
     console.log("eldo");
     var emote_element=document.createElement("a");
-    //emote_element.appendChild(document.createTextNode("Fuck"));
-    emote_element.style.left=(Math.random()*document.body.clientWidth).toFixed(0).toString()+"px";
-    emote_element.style.top="0px";
     emote_element.style.position="absolute";
     emote_element.style.fontSize="3rem";
     emote_element.style.color="dark";
     emote_element.className="fa fa-linux";
+    emote_element.style.left=((Math.random()*document.body.clientWidth).toFixed(0)-emote_element.clientWidth).toString()+"px";
+    emote_element.style.top="0px";
     document.body.appendChild(emote_element);
 
     emote_element.animate([
@@ -17,7 +16,7 @@ function emote()
             transform: 'translateY(' + window.scrollY + 'px) rotate(0deg)'
         },
         { 
-            transform: 'translateY(' + (500+window.scrollY+window.innerHeight) + 'px) rotate(360deg)'
+            transform: 'translateY(' + (window.scrollY+window.innerHeight) + 'px) rotate(360deg)'
         }
     ],{
         duration: 2000,
